1c1728d21b1272a228054553e7dc404baa04c3bf
AmanMatrix <itsamansingla11@gmail.com>
2021-10-17 17:47:18 +0530

    Minor updates

--- snakeGame.py
+++ snakeGame.py
@@ -14,15 +14,15 @@ initVelocity = grid_size
 snakeSize = grid_size
 
 fps = 15
-
+game_mode = False  # False for easy mode and True for hard mode
 
 # Score window and text
-def scoreWindow(text, color, x, y, size):
+def textWindow(text, color, x, y, size):
     font = pygame.font.SysFont(None, size)
     screen_text = font.render(text, True, color)
     gameWindow.blit(screen_text, [x, y])
 
-#Snake length and specifications
+# Snake length and specifications
 def plotSnake(gameWindow, color, snakeList, snakeSize):
     for x, y in snakeList:
         pygame.draw.rect(gameWindow, color, [x, y, snakeSize, snakeSize])
@@ -32,9 +32,9 @@ def plotSnake(gameWindow, color, snakeList, snakeSize):
 black = (0, 0, 0)
 white = (255, 255, 255)
 red = (255, 0, 0)
-blue=(0,0,255)
+blue = (0, 0, 255)
 
-#Game clock
+# Game clock
 clock = pygame.time.Clock()
 
 # Game window initialization
@@ -43,19 +43,34 @@ gameWindow = pygame.display.set_mode(
 pygame.display.set_caption('Snake Game')
 pygame.display.update()
 
-#Welcome screen
+# Welcome screen
 def welcome():
-    exit_game=False
+    global game_mode
+    exit_game = False
+    gameWindow.fill(black)
+    textWindow("Welcome to Snakes", white, 100, 200, 50)
+    textWindow('Please select game mode', white, 150, 250, 25)
+    textWindow('• Easy "E"', white, 150, 275, 25)
+    textWindow('• Hard "H"', white, 150, 300, 25)
+
     while not exit_game:
-        gameWindow.fill(black)
-        scoreWindow("Welcome to Snakes",white,100,200,50)
-        scoreWindow('Press "SpaceBar" to Play',white,150,270,25)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                    exit_game = True
-                    pygame.quit()
-                    quit()
+                exit_game = True
+                pygame.quit()
+                quit()
             if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_e:
+                    gameWindow.fill(black)
+                    textWindow("Welcome to Snakes", white, 100, 200, 50)
+                    textWindow('Press "SpaceBar" to Play', white, 150, 270, 25)
+
+                    game_mode = False
+                if event.key == pygame.K_h:
+                    gameWindow.fill(black)
+                    textWindow("Welcome to Snakes", white, 100, 200, 50)
+                    textWindow('Press "SpaceBar" to Play', white, 150, 270, 25)
+                    game_mode = True
                 if event.key == pygame.K_SPACE:
                     gameLoop()
             pygame.display.update()
@@ -80,17 +95,21 @@ def gameLoop():
     snakeLen = 1
 
     score = 0
+    with open("hiscore.txt", "r") as f:
+        hiscore = f.read()
 
     while not exit_game:
-        #Game over criteria
+        # Game over criteria
         if game_over:
+            with open("hiscore.txt", "w") as f:
+                f.write(str(hiscore))
             gameWindow.fill(black)
-            scoreWindow(f'Game Over!!!', (255, 0, 0),
-                        (screen_width/2)-125, (screen_height/2)-50, 50)
-            scoreWindow(f'Your score was {score}', (255, 0, 0),
-                        (screen_width/2)-85, (screen_height/2)-10, 25)
-            scoreWindow("Press Enter to continue...", (0, 255, 0),
-                        (screen_width/2)-110, screen_height-20, 25)
+            textWindow(f'Game Over!!!', (255, 0, 0),
+                       (screen_width/2)-125, (screen_height/2)-50, 50)
+            textWindow(f'Your score was {score}', (255, 0, 0),
+                       (screen_width/2)-85, (screen_height/2)-10, 25)
+            textWindow("Press Enter to continue...", (0, 255, 0),
+                       (screen_width/2)-110, screen_height-20, 25)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -100,36 +119,24 @@ def gameLoop():
                     if event.key == pygame.K_RETURN:
                         welcome()
         else:
-            #In-game movement
+            # In-game movement
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     exit_game = True
 
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_d:
-                        velocityX = initVelocity
-                        velocityY = 0
                     if event.key == pygame.K_RIGHT:
                         velocityX = initVelocity
                         velocityY = 0
 
-                    if event.key == pygame.K_a:
-                        velocityX = - 1 * initVelocity
-                        velocityY = 0
                     if event.key == pygame.K_LEFT:
                         velocityX = - 1 * initVelocity
                         velocityY = 0
 
-                    if event.key == pygame.K_w:
-                        velocityY = - 1 * initVelocity
-                        velocityX = 0
                     if event.key == pygame.K_UP:
                         velocityY = - 1 * initVelocity
                         velocityX = 0
 
-                    if event.key == pygame.K_s:
-                        velocityY = initVelocity
-                        velocityX = 0
                     if event.key == pygame.K_DOWN:
                         velocityY = initVelocity
                         velocityX = 0
@@ -137,16 +144,18 @@ def gameLoop():
             snakeY += velocityY
             snakeX += velocityX
 
-            #Food position and length,score update
+            # Food position and length,score update
             if snakeX == foodX and snakeY == foodY:
-                score += 1
-                snakeLen += 1
+                score += 10
+                snakeLen += 3
                 foodX = grid_size*random.randint(1, (screen_width/grid_size)-1)
                 foodY = grid_size * \
-                    random.randint(1, (screen_height/grid_size)-1)
-
+                    random.randint(10, (screen_height/grid_size)-1)
+                if score > int(hiscore):
+                    hiscore = score
             gameWindow.fill(black)
-            scoreWindow(f"Score:{score}", white, 5, 5, 25)
+            textWindow(f"Current Score:{score}", white, 5, 10, 25)
+            textWindow(f"High Score:{hiscore}", red, 380, 10, 25)
 
             head = []
             head.append(snakeX)
@@ -156,34 +165,30 @@ def gameLoop():
             if len(snakeList) > snakeLen:
                 del snakeList[0]
 
-            #Snake overlapping on itself(Game Over)
+            # Snake overlapping on itself(Game Over)
             if head in snakeList[:-1]:
                 game_over = True
+            if game_mode == False:  # Easy Mode
+                # Snake Moving out of screen
+                if snakeX < 0:
+                    snakeX = (screen_width + snakeX) + grid_size
+                    snakeY = snakeY
 
-            #Snake Moving out of screen (hard mode)
-            if snakeX <= 0 or snakeX >= screen_width or snakeY <= 0 or snakeY >= screen_height:
-               game_over =True
+                if snakeX > screen_width:
+                    snakeX = (snakeX - screen_width) - 2*grid_size
+                    snakeY = snakeY
 
+                if snakeY < 0:
+                    snakeY = (screen_width + snakeY) + grid_size
+                    snakeX = snakeX
 
-            # #Snake Moving out of screen (easy mode)
-            # if snakeX < 0:
-            #     snakeX = (screen_width + snakeX) + grid_size
-            #     snakeY = snakeY
+                if snakeY > screen_height:
+                    snakeY = (snakeY - screen_width) - 2*grid_size
+                    snakeX = snakeX
 
-            # if snakeX > screen_width:
-            #     snakeX = (snakeX - screen_width) - 2*grid_size
-            #     snakeY = snakeY
-
-            # if snakeY < 0:
-            #     snakeY = (screen_width + snakeY) + grid_size
-            #     snakeX = snakeX
-
-            # if snakeY > screen_height:
-            #     snakeY = (snakeY - screen_width) - 2*grid_size
-            #     snakeX = snakeX
-
-
-
+            elif game_mode == True:  # Hard Mode
+                if snakeX <= 0 or snakeX >= screen_width or snakeY <= 0 or snakeY >= screen_height:
+                    game_over = True
             # Position of snake
             plotSnake(gameWindow, white, snakeList, snakeSize)
 
